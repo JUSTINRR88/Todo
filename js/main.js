aff54e610507ab7fdcdb16e5a341e5ede333617a
JUSTINRR88 <126489355+JUSTINRR88@users.noreply.github.com>
2024-06-05 10:04:17 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,113 @@
+import { tareas} from './data_todo.js';
+
+function cargar_tareas(){
+
+    let cuadro_de_tareas = document.querySelector(".lista_tareas");
+
+    tareas.forEach((cada_tarea) => {
+
+        let div_tarea = document.createElement("div");
+        div_tarea.classList.add("div_tareas");
+
+        if(cada_tarea.estado){
+            div_tarea.innerHTML = `
+            <p class="texto">${cada_tarea.texto}</p>
+            <div class="estado">[/]</div>
+        `;
+        }else {
+            div_tarea.innerHTML = `
+            <p class="texto">${cada_tarea.texto}</p>
+            <div class="estado">[X]</div>
+        `;
+        }
+        cuadro_de_tareas.appendChild(div_tarea);
+    });
+}
+cargar_tareas();
+
+function cargar_botones(){
+    let caja_btn = document.querySelector(".botones");
+
+    caja_btn.innerHTML = ` <div class="btn_mas"> + </div>`;
+}
+cargar_botones();
+
+function cargar_formulario(){
+    let ventana_formulario = document.querySelector(".formulario");
+    ventana_formulario.classList.add("activar_b")
+    ventana_formulario.innerHTML = `
+        <div class="div_controles">
+            <div class="btn_cerrar">X</div>
+        </div>
+
+        <div class="div_formulario">
+            <input type="txt" class="entrada-tarea">
+            <select id="categoria" name="categoria">
+                <option value="perro">Categoría</option>
+                <option value="perro">Casa</option>
+                <option value="gato">Universidad</option>
+                <option value="pajaro">Trabajo</option>
+            </select>
+        </div>
+
+        <div class="btn-crear">Crear</div>
+    `;
+
+    let btn_cerrar2 = document.querySelector(".btn_cerrar");
+    btn_cerrar2.addEventListener("click", ()=>{
+        ventana_formulario.classList.remove("activar_b")
+    });
+
+
+
+
+
+
+
+
+    // PROGRAMACIÓN DEL BOTON CREAR
+    let btn_crear = document.querySelector(".btn-crear");
+
+    btn_crear.addEventListener("click",()=>{
+
+        // Recupera los datos del formulario para la nueva tarea
+        let tarea = document.querySelector(".entrada-tarea").value;
+
+      
+        // Organiza los datos para la nueva tarea
+        let estructura_de_tarea ={
+                estado: true,
+                id: tarea,
+                texto: tarea,
+            };
+
+        // Agrega la tarea nueva a la lista de tareas
+        tareas.push(estructura_de_tarea)
+
+        // Limpiar las tareas anteiores para actualizar
+        let cuadro_de_tareas = document.querySelector(".lista_tareas");
+        cuadro_de_tareas.innerHTML = "";
+        
+        // Carga todas las tareas nuevamente al DOM
+        cargar_tareas();
+        
+        // cierra el formulario de la nueva tarea
+        ventana_formulario.classList.remove("activar_b")
+
+
+    
+
+        
+    })
+
+
+
+
+
+}
+
+// PROGRAMACIÓN DEL BOTON
+let btn_formulario = document.querySelector(".btn_mas");
+btn_formulario.addEventListener("click", cargar_formulario)
+
+
